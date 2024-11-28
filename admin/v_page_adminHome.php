26d3f35435daeeaf9005d8625b72e1b4cafea1d8
nguyentrongnguyen <nguyentrongnguyen2507@gmail.com>
2024-11-28 08:20:09 +0700

    Create v_page_adminHome.php

---
+++ admin/v_page_adminHome.php
@@ -0,0 +1,16 @@
+<?php include 'v_page_adminHeader.php'; ?>
+<style>
+    h2 {
+        text-align: center;
+        font-size: 50px;
+        color: #0d6efd;
+    }
+</style>
+
+<h2>WELCOME TO ADMIN PAGE</h2>
+
+            
+<?php include 'v_page_adminFooter.php'; ?>
+
+
+
